dcf8372fc02c2f32e7ecdd92ba0c8f4050074bd2
angelo <angelomadara.dev@gmail.com>
2020-03-28 17:34:38 +0800

    google crawl

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="text-center">Covid19 Update</h1>
+    <h1 class="text-center">Corona Virus 2019 Update</h1>
     <div>
       <p>API source: <a href="https://documenter.getpostman.com/view/8854915/SzS7R6uu?version=latest" target="_blank">Github repo</a> or check the documetation on <a href="https://documenter.getpostman.com/view/8854915/SzS7R6uu?version=latest" target="_blank">postman</a>.</p>
     </div>
