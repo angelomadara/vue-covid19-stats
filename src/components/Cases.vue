81e714b19523916960d0dec53e50d63fca5c35a0
angelo <angelomadara.dev@gmail.com>
2020-03-28 22:12:46 +0800

    table sticky

--- src/components/Cases.vue
+++ src/components/Cases.vue
@@ -16,7 +16,7 @@
 
     <b-table 
       id="country-table" 
-      sticky-header small striped hover bordered
+      small striped hover bordered sticky-header
       :items="cases" 
       :fields="fields"
       :filter="filter"
@@ -71,6 +71,8 @@ export default {
   #country-cases-holder{
     height: 497px;
     overflow-y: scroll;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
   }
   #country-table{
     border-collapse: collapse; 
@@ -79,5 +81,8 @@ export default {
     position: sticky;
     top: 0;
     background: #fff;
+    cursor: pointer;
+    /* border-bottom: 10px solid #ddd; */
+    box-shadow: 0px -10px  0px #ddd, 0px 5px  10px #ddd;
   }
 </style>
